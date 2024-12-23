75d4f7f9029475e43817e343b30eaebd655b1520
Heta <hhmj28@gmail.com>
2024-12-23 23:57:11 +0200

    kuvat

--- myPage/src/components/Scolopendra.jsx
+++ myPage/src/components/Scolopendra.jsx
@@ -2,8 +2,8 @@ function Scolopendra() {
     return (
         <div>
             <div className="image-row">
-            <img src="/src/assets/pics/s1.jpg" alt="snail1" className="snail" />
-            <img src="/src/assets/pics/s2.jpg" alt="snail2" className="snail" />
+            <img src="/assets/pics/s1.jpg" alt="snail1" className="snail" />
+            <img src="/assets/pics/s2.jpg" alt="snail2" className="snail" />
             </div>
             <h1>Scolopendra</h1>
             <p>Omg mun lemppari</p>
